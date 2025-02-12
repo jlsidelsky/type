eb66d6213c5495c30cb26851cfbaf3e4f233d861
jlsidelsky <j.l.sidelsky@wustl.edu>
2025-02-12 00:56:23 -0600

    should be it

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/jlsidelsky/", // Replace with your repository name
+  base: "/type/", // Replace with your repository name
 });
